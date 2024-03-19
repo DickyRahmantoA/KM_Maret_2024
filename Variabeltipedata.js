a2187ab658a55221a45047f3f0f7dae0f17bc5af
Dicky Rahmanto <dickyrahmantojr@gmail.com>
2024-03-19 21:41:47 +0700

    Menambahkan Variabeltipedata

---
+++ Variabeltipedata.js
@@ -0,0 +1,32 @@
+// Variable dan TIPE DATA
+
+// var number = 10;
+// console.log(number);
+
+// var str = "Buku";
+// console.log(str);
+
+// var isCorrect = true;
+// console.log(isCorrect)
+
+// Operatir Math
+
+var a = 10;
+var b = 'xyz';
+var total = a - b;
+
+// NaN = not a number
+// typeof untuk cek tipe data
+
+// console.log( typeof total);
+
+// OPERATOR LOGIC
+
+var x = 10;
+var y = 10;
+
+var z = x == y;
+console.log(z)
+
+// == membandingkan value saja
+// === membandingkan value dan tipe data
