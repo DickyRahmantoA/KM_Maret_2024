b4fe6ae740ba6425ed30bf8495791f442788780a
Dicky Rahmanto <dickyrahmantojr@gmail.com>
2024-03-19 20:35:42 +0700

    Menambahkan Algoritma dan Pseudocode

---
+++ index,.js
@@ -0,0 +1,95 @@
+/* 
+ Study Case 1
+ Tentukan algoritma dan pseudocode untuk menghitung keliling persegi panjang
+
+ Algoritma keliling Persegi Panjang
+ Deklarasi 
+    panjang : integer
+    lebar : integer
+ 
+ Deskripsi 
+    read (panjang)
+    read (lebar)
+    K <- 2*(panjang + lebar)
+    write(K)
+
+    algoritma coach
+    1. buat variable panjang
+    2. buat variable lebar
+    3. buat variable keliling
+    4. masukan rumus keliling = 2 x panjang + 2 x lebar
+    5. cetak keliling
+    
+    pseudocode coach
+
+    STORE panjang WITH ANY NUMBER
+    STORE lebar WITH ANY NUMBER
+
+    STORE keliling WITH 2 MUL panjang ADD 2 MUL lebar
+    DISPLAY keliling
+    
+
+    Study Case 2
+    Tentukan algoritma dan pseudocode untuk menghitung luas permukaan tabung
+
+    Algoritma luas permukaan tabung
+    Deklarasi
+        jari-jari : real
+        tinggi : real
+    Deskripsi
+        read (jari-jari)
+        read (tinggi)
+        L <- 2*3.14*jari-jari*(jari-jari+tinggi)
+        write(L)
+
+
+        ALgoritma coach
+
+        1. cari luas permukaan tabung = 2 x pi x r x r + 2 x pi x r x t
+        2. buat variabel pi
+        3. buat varibel r
+        4. buat variabel t
+        5. buat variabel luasPermukaan
+        6. isi luas permukaan dengan rumus = 2 x pi x r x r + 2 x pi x r x t
+        7. cetak luasPermukaan
+
+        pseudocode coach
+
+        STORE pi WITH 3.14
+        STORE r WITH ANY NUMBER
+        STORE t WITH ANY NUMBER
+
+        STORE luasPermukaan WITH 2 MUL pi MUL r MUL r ADD 2 MUL pi MUL r MUL t
+        DISPLAY luasPermukaan
+
+
+    Study Case 3
+    Tentukan algoritma dan pseudocode volume 1/4 volume bola
+
+    Algoritma volume 1/4 volume bola
+    Deklarasi
+        jari-jari : real
+    Deskripsi
+        read (jari-jari)
+        V <- 0,16*3,14*jari-jari*jari-jari*jari-jari
+        write(V)
+
+    
+    Algoritma Coach
+    1. cari rumus volume bola = 4/3 x pi x r x r x r
+    2. buat variabel pi
+    3. buat variabel r
+    4. buat variabel volume
+    5. isi volume dengan rumus = 1/4 x 4/3 x pi x r x r x r
+    6. cetak volume
+
+    pseudocode coach
+    STORE pi WITH 3.14
+    STORE r WITH ANY NUMBER
+
+    STORE volume WITH 1/4 MUL 4/3 MUL pi MUL r MUL r MUL r
+    DISPLAY volume
+
+
+
+*/
